122678653c4fe7657464c44ae6dead1892a3eb1e
Maria_Xavier <mcs_nasc@hotmail.com>
2022-04-07 13:40:55 -0300

    JSON - JavaScript Object Notation

---
+++ JSON/index.php
@@ -0,0 +1,29 @@
+<?php 
+
+echo "JSON";
+
+//NÃO SE USA <BR>, ESTOU USANDO AQUI PARA FINS ACADEMICOS
+
+echo "<br><br>";
+
+//JSON (JavaScript Object Notation) --> Mecanismo para transporte de informações
+
+/*
+
+https://jsonformatter.curiousconcept.com/
+
+{
+    "firstname" : "Gunnar",
+    //chave         nome
+    "lastname" : "Correa"
+}
+
+{
+    "titulo" : "Titanic",
+    "sinopse" : "Um navio colide com um iceberg e afunda",
+    "ano" : 2017
+    "horarios" : ["16:00" , "19:00" , "20:00"]
+}
+
+*/
+?>
