7e575660a47f9a962164b27d76f62edb5d8e1e40
Maria Xavier <mcs_nasc@hotmail.com>
2022-04-25 15:48:55 -0300

    Funções com Parâmetros

--- FuncoesParametros/index.php
+++ FuncoesParametros/index.php
@@ -6,6 +6,28 @@ echo "FUNÇÕES COM PARAMETROS DE REFERENCIA";
 
 echo "<br><br>";
 
+function ola ($texto = "mundo", $periodo = "Bom dia"){
+    return "Olá, $texto! $periodo <br>";
+}
+
+echo ola();
+echo ola("Maria");
+echo ola(" ", "Boa noite");
+echo ola("Fulano");
+echo ola("Jubileu", "Boa tarde");
+echo ola("Xavier", " ");
+
+echo "<br><br>";
+
+function ola2 (){
+    $argumentos = func_get_args();
+    return $argumentos;
+}
+
+var_dump(ola2("Bom dia"));
+
+echo "<br><br>";
+
 $salario = 1567.76;
 $valorAjuste = 0;
 
