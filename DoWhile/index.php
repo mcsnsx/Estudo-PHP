6cabbc5efb336a95ed2af0c519b68bd0ad196bc9
Maria Xavier <mcs_nasc@hotmail.com>
2022-04-25 11:19:04 -0300

    DoWhile

--- DoWhile/index.php
+++ DoWhile/index.php
@@ -13,6 +13,17 @@ do{
     $i++;
 } while ($i < 10);
 
+echo "<br><br>";
+
+$total = 150;
+$desconto = 0.9;
+
+do{
+    $total *= $desconto;
+} while ($total > 100);
+
+echo $total;
+
 ?>
 
 <!DOCTYPE html>
