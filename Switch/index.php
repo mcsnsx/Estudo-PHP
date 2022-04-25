a772ca3a4fb49da84dfa3e610f7050ebcff51b43
Maria Xavier <mcs_nasc@hotmail.com>
2022-04-25 09:51:13 -0300

    Switch - case

--- Switch/index.php
+++ Switch/index.php
@@ -33,6 +33,44 @@ switch($dia){
     default:
         $msg = "Não encontrado";
 }
+
+$diaDaSemana = 8; //date ("w");
+// 'w' --> dia da semana
+
+switch($diaDaSemana){
+    case 0:
+        echo "Domingo";
+    break;
+
+    case 1:
+        echo "Segunda-feira";
+    break;
+
+    case 2:
+        echo "Terça-feira";
+    break;
+
+    case 3:
+        echo "Quarta-feira";
+    break;
+
+    case 4:
+        echo "Quinta-feira";
+    break;
+
+    case 5:
+        echo "Sexta-feira";
+    break;
+
+    case 6:
+        echo "Sabado";
+    break;
+
+    default:
+    echo "Data inválida!";
+
+}
+
 ?>
 
 <!DOCTYPE>
