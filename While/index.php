1144f26ad4b4a0ebba6c567519e8f8d1c789c93e
Maria Xavier <mcs_nasc@hotmail.com>
2022-04-25 11:10:33 -0300

    While

--- While/index.php
+++ While/index.php
@@ -7,11 +7,27 @@ echo "WHILE";
 echo "<br><br>";
 
 $cont = 0;
+
+//WHILE --> Enquanto, "faça essa ação enquanto..", vai executar enquanto a condição for verdadeira
 while ($cont < 20){
     echo "Cont: {$cont}<br>";
     $cont++;
     // OU $cont += 1;
     // OU $cont = $cont + 1; 
+}
+
+echo "<br><br>";
+
+$condicao = true;
+
+while($condicao){
+    //rand --> função que escolhe aleatoriamente, random
+    $numero = rand (1, 10);
+    if ($numero === 3){
+        echo "TRÊS!!!<br>";
+        $condicao = false;
+    }
+    echo $numero . " ";
 }
 ?>
 
