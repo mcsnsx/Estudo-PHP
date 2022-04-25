223bb23cfd5c0afc5b58c0c80873fc6cd14ee7b7
Maria Xavier <mcs_nasc@hotmail.com>
2022-04-25 10:16:04 -0300

    Laço - For

--- For/index.php
+++ For/index.php
@@ -6,9 +6,35 @@ echo "FOR";
 
 echo "<br><br>";
 
+echo "De 0 a 9";
+echo "<br><br>";
+
+
 for ($i = 0; $i < 10; $i++) {
     echo $i . "<br>";
 }
+
+echo "<br><br>";
+
+echo "De 0 a 1000, de 5 em 5";
+echo "<br><br>";
+
+for ($f = 0; $f < 1000; $f+=5) {
+    if ($f>200 && $f < 800) continue;
+    if ($f === 900) break;
+    echo $f . "<br>";
+}
+
+echo "Anos";
+echo "<br><br>";
+
+echo "<select>";
+
+for ($g =  date ("Y"); $g > date ("Y") - 100; $g--) {
+    echo '<option value="' . $g . '">' . $g . '<option><br>';
+}
+
+echo "</select>";
 
 ?>
 
