5abfe3ec1daf37a31fcf260697aced15d822bdb6
Maria Xavier <mcs_nasc@hotmail.com>
2022-04-25 11:42:37 -0300

    Array

--- Array/index.php
+++ Array/index.php
@@ -112,4 +112,58 @@ for ($i = 0; $i <= 100; $i++){
     echo "2 * {$i} = " . (2 * $arr[$i]) . "<br>";
 }
 
+echo "<br><br>";
+
+echo "VETOR";
+//VETOR ---> quando se trata de um array sem mais dimensões, apenas uma lista
+
+echo "<br><br>";
+
+$frutas = array (
+    "laranja",
+    "abacaxi",
+    "melancia"
+);
+
+//PRINT_R ---> similar ao 'echo'
+print_r($frutas);
+
+echo "<br><br>";
+
+echo "ARRAY BIDIMENSIONAL";
+
+echo "<br><br>";
+
+$carros [0][0] = "GM";
+$carros [0][1] = "Cobalt";
+$carros [0][2] = "Onix";
+$carros [0][3] = "Camaro";
+
+$carros [1][0] = "Ford";
+$carros [1][1] = "Fiesta";
+$carros [1][2] = "Fusion";
+$carros [1][3] = "Eco Sport";
+
+echo $carros [0][3];
+
+echo "<br><br>";
+
+echo end($carros[1]);
+
+echo "<br><br>";
+
+$pessoas = array();
+
+array_push($pessoas, array(
+    'nome'=>'João',
+    'idade'=>20
+));
+
+array_push($pessoas, array(
+    'nome'=>'Glaucio',
+    'idade'=>25
+));
+
+print_r($pessoas[0]['nome']);
+
 ?>
