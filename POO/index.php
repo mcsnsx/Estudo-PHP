fb220220bd9f339b5e7319638a85b30be2fb396a
Maria Xavier <mcs_nasc@hotmail.com>
2022-04-26 12:34:47 -0300

    Programação Orientada a Objetos - POO

---
+++ POO/index.php
@@ -0,0 +1,58 @@
+<?php
+
+//NÃO SE USA <BR>, ESTOU USANDO AQUI PARA FINS EDUCACIONAIS
+
+echo "CONCEITO DE PROGRAMAÇÃO ORIENTADA A OBJETOS - POO";
+//é um estilo de programação 
+
+echo "<br><br>";
+
+echo "<b>O que é uma classe?</b>
+      <p>É um conjunto de atributos e métodos. Funciona como uma caixa e o que 
+      importa é o que está dentro dela, se refere a diferentes assuntos, cada 
+      classe agrupa um 'assunto' dentro dela onde só será 'discutido aquele assunto'.
+      Dentro das classes, as variáveis e funções gnham alguns recursos. Quando for 
+      preciso armazenar valores dentro de uma classe, pode-se criar um atributo."
+;
+
+echo "<br><br>";
+
+echo "<b>O que é um atributo?</b>
+      <p>Funciona como uma variável mas com recursos a mais. Quando uma variável está
+      dentro de ua classe, ela passa a ser um atributo, isso é chamado de 'Encapsulamento'.
+      É onde eu deixo as informações quando eu preciso armazena-las."
+;
+
+echo "<br><br>";
+
+echo "<b>O que é um encapsulamento?</b>
+      <p>É uma forma de controle de acesso sobre as funções e variaveis/atributos dentro da 
+      classe."
+;
+
+echo "<br><br>";
+
+echo "<b>O que é um método?</b>
+      <p>É como uma função. É onde eu deixo as informações quando eu preciso executa-las. "
+;
+
+echo "<br><br>";
+
+echo "<b>O que é um objeto?</b>
+      <p>É uma variável que representa ou copia a classe, é representada a partir da palavra
+      'new' que é o que cria/inicia a classe de fato."
+;
+
+echo "<br><br>";
+
+echo "<b>O que é um instanciamento?</b>
+      <p>Quando vamos usar uma classe, precisamos fazer o instanciamento que é a forma de
+      representar/chamar a classe, funciona como uma referencia/especificação, faz a classe
+      funcionar "
+;
+
+echo "<br><br>";
+
+echo "<b>O que é herança?</b>
+      <p>É quando uma classe herda informações de outra classe"
+;
