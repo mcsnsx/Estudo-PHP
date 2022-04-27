f7387cd67c61335358f0ceefb940f75713a074b3
Maria Xavier <mcs_nasc@hotmail.com>
2022-04-27 12:51:09 -0300

    Herança

---
+++ Heranca/index.php
@@ -0,0 +1,42 @@
+<?php
+
+//NÃO SE USA <BR>, ESTOU USANDO AQUI PARA FINS EDUCACIONAIS
+
+echo "HERANÇA";
+//é um estilo de programação 
+
+echo "<br><b>Classes primárias e secundarias</b>";
+//ABSTRAÇÃO --> fazer classes genericas que podem conter atributos correspondentes a outras classes
+
+echo "<br><br>";
+
+class Documento{
+    private $numero;
+
+    public function getNumero(){
+        return $this-> numero;
+    }
+    
+    public function setNumero($n){
+        $this-> numero = $n;
+    }
+}
+
+class CPF extends Documento{
+    public function validar():bool{
+        //Validação do CPF
+        return true;
+    }
+}
+
+$doc = new CPF();
+
+$doc -> setNumero("12345678900");
+
+var_dump($doc -> validar());
+
+echo "<br><br>";
+
+echo $doc->getNumero();
+
+?>
