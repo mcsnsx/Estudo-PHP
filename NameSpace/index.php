353d1ce5701ccde72a4024ad9d45d3ad8448482d
Maria Xavier <mcs_nasc@hotmail.com>
2022-04-28 15:24:44 -0300

    Organizando Classes

---
+++ NameSpace/index.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once("config.php");
+
+$cad = new Cadastro();
+
+$cad->setNome("Djalma Sindeaux");
+$cad->setEmail("djalma@hcode.com.br");
+$cad->setSenha("123456");
+
+echo $cad;
+
+?>
