e4a9728a3ecede4eb72bde6591684f4e75e3acd4
Maria Xavier <mcs_nasc@hotmail.com>
2022-04-28 15:41:53 -0300

    Classe de Casdastro do Cliente

--- NameSpace/class/Cliente/Cadastro.php
+++ NameSpace/class/Cliente/Cadastro.php
@@ -1,6 +1,7 @@
 <?php
 
 //Raiz das informações
+//NAMESPACE ---> Usado para referenciar qual espaço/diretorio a informação está
 namespace Cliente;
 
 //usar a contra-barra '\' significa "volte da raiz"
