66071dcbdc157b4e1599f039b1f8cf90c1486779
Maria Xavier <mcs_nasc@hotmail.com>
2022-04-28 15:35:50 -0300

    Organizando Classes

--- NameSpace/index.php
+++ NameSpace/index.php
@@ -2,12 +2,15 @@
 
 require_once("config.php");
 
+//forma de referenciar o arquivo
+use Cliente\Cadastro;
+
 $cad = new Cadastro();
 
 $cad->setNome("Djalma Sindeaux");
 $cad->setEmail("djalma@hcode.com.br");
 $cad->setSenha("123456");
 
-echo $cad;
+$cad->registrarVenda();
 
 ?>
