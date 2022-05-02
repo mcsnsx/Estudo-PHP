d86bdd7d12f4e2eca86e3f3fc184bddb01d8f255
Maria Xavier <mcs_nasc@hotmail.com>
2022-05-02 16:47:23 -0300

    Conexão MySQL - Atualizando Dados

---
+++ PDO/index_04.php
@@ -0,0 +1,29 @@
+<?php
+
+//NÃO SE USA <BR>, ESTOU USANDO AQUI PARA FINS EDUCACIONAIS
+
+echo "CONEXÃO - SQL Server";
+//Padrão de comandos para interagir com o banco de dados
+
+echo "<br><br>";
+
+$conn = new PDO ("mysql:host=localhost;dbname=dbphp8", "root", "" );
+
+//UPDAE ---> Atualizar
+$stmt = $conn -> prepare ("UPDATE tb_usuarios SET deslogin = :LOGIN, dessenha = :PASSWORD WHERE idusuario = :ID");
+
+$login = "joao";
+$password = "qwerty";
+$id = 2;
+
+//bindParam ---> linka as informações
+$stmt -> bindParam(":LOGIN", $login);
+$stmt -> bindParam(":PASSWORD", $password);
+$stmt -> bindParam(":ID", $id);
+
+$stmt -> execute();
+
+echo "Alterado OK!";
+
+
+?>
