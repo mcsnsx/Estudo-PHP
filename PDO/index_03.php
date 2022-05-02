ddbf13e5977bf85c8e0dac904395aecd1a37ed8b
Maria Xavier <mcs_nasc@hotmail.com>
2022-05-02 16:46:35 -0300

    Conexão MySQL - Inserindo Dados

---
+++ PDO/index_03.php
@@ -0,0 +1,25 @@
+<?php
+
+//NÃO SE USA <BR>, ESTOU USANDO AQUI PARA FINS EDUCACIONAIS
+
+echo "MySQL - Inserindo Dados";
+//Padrão de comandos para interagir com o banco de dados
+
+echo "<br><br>";
+
+$conn = new PDO ("mysql:host=localhost;dbname=dbphp8", "root", "" );
+
+$stmt = $conn -> prepare ("INSERT INTO tb_usuarios (deslogin, dessenha) VALUES (:LOGIN, :PASSWORD)");
+
+$login = "jose";
+$password = "1234567890";
+
+//bindParam ---> linka as informações
+$stmt -> bindParam(":LOGIN", $login);
+$stmt -> bindParam(":PASSWORD", $password);
+
+$stmt -> execute();
+
+echo "Inserido OK!";
+
+?>
