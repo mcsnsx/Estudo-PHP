fcc4810b8c62ae0a24fa25d630600f3b91cb4197
Maria Xavier <mcs_nasc@hotmail.com>
2022-05-03 16:19:07 -0300

    DAO - Conexão Banco de Dados

--- DAO/index.php
+++ DAO/index.php
@@ -10,10 +10,30 @@ $usuarios = $sql -> select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios);
 */
 
+/*
+//CARREGA APENAS UM USUÁRIO
 $root = new Usuario();
-
 $root -> loadById(3);
-
 echo $root;
+*/
+
+/*
+//CARREGA UMA LISTA DE UUARIOS
+$lista = Usuario::getList();
+echo json_encode($lista);
+*/
+
+/*
+//CARREGA UMA LSTA DE USUARIOS BUSCANDO PELO LOGIN
+//busca os usuarios que tem 'jo' no login
+$search = Usuario::search("jo");
+echo json_encode($search);
+*/
+
+
+//CARREGA UM USUARIO USANDO O LOGIN E A SENHA 
+$usuario = new Usuario();
+$usuario->login("root", "!@#$");
+echo $usuario;
 
 ?>
